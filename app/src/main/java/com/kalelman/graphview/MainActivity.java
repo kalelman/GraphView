f0a78b79c7f7c289d106f021a9b4a8d7a6f18995
kalelman <erick_rojas_perez@hotmail.com>
2018-12-03 12:32:39 -0500

    Implementation of new funcionalityes our graph

--- app/src/main/java/com/kalelman/graphview/MainActivity.java
+++ app/src/main/java/com/kalelman/graphview/MainActivity.java
@@ -3,11 +3,65 @@ package com.kalelman.graphview;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+import com.jjoe64.graphview.series.PointsGraphSeries;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.graph)
+    GraphView graph;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        // activate horizontal zooming and scrolling
+        //graph.getViewport().setScalable(true);
+
+        // activate horizontal scrolling
+        //graph.getViewport().setScrollable(true);
+
+        // activate horizontal and vertical zooming and scrolling
+        //graph.getViewport().setScalableY(true);
+
+        // activate vertical scrolling
+        //graph.getViewport().setScrollableY(true);
+
+        // set manual X bounds
+        /*graph.getViewport().setXAxisBoundsManual(true);
+        graph.getViewport().setMinX(0.5);
+        graph.getViewport().setMaxX(3.5);
+
+        // set manual Y bounds
+        graph.getViewport().setYAxisBoundsManual(true);
+        graph.getViewport().setMinY(3.5);
+        graph.getViewport().setMaxY(8);*/
+
+        /*LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(-1, -1),
+                new DataPoint(0, 0),
+                new DataPoint(1, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);*/
+
+        // Grafica usando puntos
+        PointsGraphSeries<DataPoint> series = new PointsGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, -2),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
+        series.setShape(PointsGraphSeries.Shape.POINT);
     }
 }
